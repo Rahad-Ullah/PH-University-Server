c1724754bc1b840ff329b1bf0161eff4ea850cc6
Rahad-Ullah <myemail.abc20@gmail.com>
2024-06-01 16:26:04 +0600

    Academic Semester Model defined

--- src/app/modules/academicSemester/academicSemester.model.ts
+++ src/app/modules/academicSemester/academicSemester.model.ts
@@ -1,8 +1,50 @@
-import { Schema } from "mongoose";
-import { TAcademicSemester } from "./academicSemester.interface";
+import { Schema, model } from 'mongoose';
+import { TAcademicSemester, TMonths } from './academicSemester.interface';
 
-export const academicSemesterModel = new Schema<TAcademicSemester>(
-    {
+const months: TMonths[] = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
 
-    }
-)
+const academicSemesterSchema = new Schema<TAcademicSemester>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    year: {
+      type: Date,
+      required: true,
+    },
+    code: {
+      type: String,
+      required: true,
+    },
+    startMonth: {
+      type: String,
+      enum: months,
+    },
+    endMonth: {
+      type: String,
+      enum: months,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const AcademicSemester = model<TAcademicSemester>(
+  'AcademicSemester',
+  academicSemesterSchema,
+);
