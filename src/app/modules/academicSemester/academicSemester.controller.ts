ad6ed4f60bbaba94851f4da1514161bcb0435e49
Rahad-Ullah <myemail.abc20@gmail.com>
2024-06-01 17:53:33 +0600

    Academic Semester Services created

--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -3,15 +3,17 @@ import httpStatus from 'http-status';
 import sendResponse from '../../utils/sendResponse';
 import { RequestHandler } from 'express';
 import catchAsync from '../../utils/catchAsync';
+import { academicSemesterService } from './academicSemester.service';
 
 const createAcademicSemester: RequestHandler = catchAsync(async (req, res) => {
-//   const { password, student: studentData } = req.body;
-//   const result = await UserServices.createStudentIntoDB(password, studentData);
+  const result = await academicSemesterService.createAcademicSemesterIntoDB(
+    req.body,
+  );
 
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
-    message: 'Student is created successfully',
+    message: 'Academic semester is created successfully',
     data: result,
   });
 });
--- src/app/modules/academicSemester/academicSemester.route.ts
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -1,8 +1,14 @@
 import express from 'express';
 import { AcademicSemesterControllers } from './academicSemester.controller';
+import validateRequest from '../../middlewares/validateRequest';
+import { createAcademicSemesterValidationSchema } from './academicSemester.validation';
 
 const router = express.Router();
 
-router.post('/create-academic-semester', AcademicSemesterControllers.createStudent)
+router.post(
+  '/create-academic-semester',
+  validateRequest(createAcademicSemesterValidationSchema),
+  AcademicSemesterControllers.createStudent,
+);
 
 export const AcademicSemesterRoutes = router;
