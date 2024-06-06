20c6dfa632b743e6ec76b2cf8812fb909bf409ab
Rahad-Ullah <myemail.abc20@gmail.com>
2024-06-06 13:14:25 +0600

    Validate doc existance in DB before creating and updating

--- src/app/modules/academicDepartment/academicDepartment.model.ts
+++ src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -10,14 +10,42 @@ const academicDepartmentSchema = new Schema<TAcademicDepartment>(
       unique: true,
     },
     academicFaculty: {
-        type: Schema.Types.ObjectId,
-        ref: 'AcademicFaculty'
-    }
+      type: Schema.Types.ObjectId,
+      ref: 'AcademicFaculty',
+    },
   },
   {
     timestamps: true,
   },
 );
 
+// validate doc existence in DB for create
+academicDepartmentSchema.pre('save', async function (next) {
+  const isDepartmentExist = await AcademicDepartment.findOne({
+    name: this.name,
+  });
 
-export const AcademicDepartment = model<TAcademicDepartment>('AcademicDepartment', academicDepartmentSchema);
+  if (isDepartmentExist) {
+    throw new Error('Department already exist!');
+  }
+
+  next();
+});
+
+// validate doc existence in DB for update
+academicDepartmentSchema.pre('findOneAndUpdate', async function (next) {
+  const query = this.getQuery();
+
+  const isDepartmentExist = await AcademicDepartment.findOne(query);
+
+  if (!isDepartmentExist) {
+    throw new Error('This department does not exist');
+  }
+
+  next();
+});
+
+export const AcademicDepartment = model<TAcademicDepartment>(
+  'AcademicDepartment',
+  academicDepartmentSchema,
+);
