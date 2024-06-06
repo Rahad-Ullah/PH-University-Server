5f12b6f13d2c607157f6950ecb62073faa011181
Rahad-Ullah <myemail.abc20@gmail.com>
2024-06-06 12:45:18 +0600

    Academic Department routes defined

---
+++ src/app/modules/academicDepartment/academicDepartment.route.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
+import { AcademicDepartmentControllers } from './academicDepartment.controller';
+import { createAcademicDepartmentValidationSchema, updateAcademicDepartmentValidationSchema } from './academicDepartment.validation';
+
+const router = express.Router();
+
+router.post(
+  '/create-academic-department',
+  validateRequest(createAcademicDepartmentValidationSchema),
+  AcademicDepartmentControllers.createAcademicDepartment,
+);
+
+router.get('/', AcademicDepartmentControllers.getAllAcademicDepartments);
+
+router.get('/:id', AcademicDepartmentControllers.getAcademicDepartment);
+
+router.patch(
+  '/:id',
+  validateRequest(updateAcademicDepartmentValidationSchema),
+  AcademicDepartmentControllers.updateAcademicDepartment,
+);
+
+export const AcademicDepartmentRoutes = router;
