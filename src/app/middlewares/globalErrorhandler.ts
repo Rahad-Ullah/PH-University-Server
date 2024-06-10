d3f5e295451e4d55dcecc96087bdb09214b3f86d
Rahad-Ullah <myemail.abc20@gmail.com>
2024-06-10 17:29:59 +0600

    Handle Mongoose Validation Error

--- src/app/middlewares/globalErrorhandler.ts
+++ src/app/middlewares/globalErrorhandler.ts
@@ -3,9 +3,10 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { NextFunction, Request, Response } from 'express';
 import { ZodError } from 'zod';
-import { TErrorSource } from '../interface/error';
+import { TErrorSources } from '../interface/error';
 import config from '../config';
 import { handleZodError } from '../errors/handleZodError';
+import handleValidationError from '../errors/handleValidationError';
 
 const globalErrorhandler = (
   err: any,
@@ -16,7 +17,7 @@ const globalErrorhandler = (
   let statusCode = err.statusCode || 500;
   let message = err.message || 'Something went wrong';
 
-  let errorSources: TErrorSource = [
+  let errorSources: TErrorSources = [
     {
       path: '',
       message: 'Something went wrong',
@@ -28,13 +29,17 @@ const globalErrorhandler = (
     statusCode = simplifiedError?.statusCode;
     message = simplifiedError?.message;
     errorSources = simplifiedError?.errorSources;
+  } else if (err?.name === 'ValidationError') {
+    const simplifiedError = handleValidationError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
   }
 
   return res.status(statusCode).json({
     success: false,
     message,
     errorSources,
-    err,
     stack: config.NODE_ENV === 'development' ? err?.stack : null,
   });
 };
