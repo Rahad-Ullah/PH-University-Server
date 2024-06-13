cd003fd2b65435753143d62fe6d46f4c34b4cdd5
Rahad-Ullah <myemail.abc20@gmail.com>
2024-06-13 18:51:38 +0600

    Course Validation schema

---
+++ src/app/modules/course/course.validation.ts
@@ -0,0 +1,20 @@
+import { z } from 'zod';
+
+const preRequisiteCoursesValidationSchema = z.object({
+  course: z.string(),
+  isDeleted: z.boolean().optional(),
+});
+
+const createCourseValidationSchema = z.object({
+  body: z.object({
+    title: z.string().min(1),
+    prefix: z.string().min(1),
+    code: z.number().min(1),
+    credits: z.number().min(1),
+    preRequisiteCourses: z.array(preRequisiteCoursesValidationSchema),
+  }),
+});
+
+export const CourseValidations = {
+  createCourseValidationSchema,
+};
