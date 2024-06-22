2e6ad732c5fdc0bab44cdb9441cec0e4440fbb7f
Rahad-Ullah <myemail.abc20@gmail.com>
2024-06-22 15:42:50 +0600

    Auth gaurd

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -3,17 +3,21 @@ import { UserControllers } from './user.controller';
 import validateRequest from '../../middlewares/validateRequest';
 import { createStudentValidationSchema } from '../student/student.validation';
 import { createFacultyValidationSchema } from '../faculty/faculty.validation';
+import auth from '../../middlewares/auth';
+import { USER_ROLE } from './user.constant';
 
 const router = Router();
 
 router.post(
   '/create-student',
+  auth(USER_ROLE.admin),
   validateRequest(createStudentValidationSchema),
   UserControllers.createStudent,
 );
 
 router.post(
   '/create-faculty',
+  auth(USER_ROLE.admin),
   validateRequest(createFacultyValidationSchema),
   UserControllers.createFaculty,
 );
