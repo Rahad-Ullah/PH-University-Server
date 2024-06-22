09db4f342cb852e8a3dbf2eb3673ee803d30edf1
Rahad-Ullah <myemail.abc20@gmail.com>
2024-06-22 18:47:30 +0600

    Unauthorize blocked user

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -5,6 +5,7 @@ import httpStatus from 'http-status';
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import config from '../config';
 import { TUserRole } from '../modules/user/user.interface';
+import { User } from '../modules/user/user.model';
 
 const auth = (...requiredRoles: TUserRole[]) => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
@@ -15,32 +16,37 @@ const auth = (...requiredRoles: TUserRole[]) => {
       throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized!');
     }
 
-    // check if the token is valid
-    jwt.verify(
+    // verify token
+    const decoded = jwt.verify(
       token,
       config.jwt_access_secret as string,
-      function (err, decoded) {
-        // err
-        if (err) {
-          throw new AppError(
-            httpStatus.UNAUTHORIZED,
-            'You are not authorized!',
-          );
-        }
+    ) as JwtPayload;
 
-        // check user role
-        const userRole = (decoded as JwtPayload).role;
+    // check user role
+    const { role, userId, iat } = decoded;
 
-        if (requiredRoles && !requiredRoles.includes(userRole)) {
-          throw new AppError(
-            httpStatus.UNAUTHORIZED,
-            'You are not authorized!',
-          );
-        }
-        // decoded
-        req.user = decoded as JwtPayload;
-      },
-    );
+    // checking if the user exist
+    const user = await User.isUserExistByCustomId(userId);
+
+    if (!user) {
+      throw new AppError(httpStatus.NOT_FOUND, 'User not found');
+    }
+
+    // checking if the user already deleted
+    if (user?.isDeleted) {
+      throw new AppError(httpStatus.FORBIDDEN, 'This user is deleted');
+    }
+
+    // checking if the user is blocked
+    if (user?.status === 'blocked') {
+      throw new AppError(httpStatus.FORBIDDEN, 'This user is blocked');
+    }
+
+    if (requiredRoles && !requiredRoles.includes(role)) {
+      throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized!');
+    }
+    // decoded
+    req.user = decoded as JwtPayload;
 
     next();
   });
--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -3,6 +3,7 @@ import catchAsync from '../../utils/catchAsync';
 import sendResponse from '../../utils/sendResponse';
 import { authServices } from './auth.service';
 
+// login user
 const loginUser = catchAsync(async (req, res) => {
   const result = await authServices.loginUser(req.body);
 
@@ -14,6 +15,7 @@ const loginUser = catchAsync(async (req, res) => {
   });
 });
 
+// change password
 const changePassword = catchAsync(async (req, res) => {
   const { ...passwordData } = req.body;
 
