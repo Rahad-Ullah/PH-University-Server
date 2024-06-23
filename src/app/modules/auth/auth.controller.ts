f675086002100d8077a1b8094864720bccdfced9
Rahad-Ullah <myemail.abc20@gmail.com>
2024-06-23 11:01:36 +0600

    generate refresh token and set it to cookie

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -2,16 +2,26 @@ import httpStatus from 'http-status';
 import catchAsync from '../../utils/catchAsync';
 import sendResponse from '../../utils/sendResponse';
 import { authServices } from './auth.service';
+import config from '../../config';
 
 // login user
 const loginUser = catchAsync(async (req, res) => {
   const result = await authServices.loginUser(req.body);
+  const { refreshToken, accessToken, needsPasswordChange } = result;
+
+  res.cookie('refreshToken', refreshToken, {
+    secure: config.NODE_ENV === 'production',
+    httpOnly: true,
+  });
 
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
     message: 'User logged in successfully',
-    data: result,
+    data: {
+      accessToken,
+      needsPasswordChange,
+    },
   });
 });
 
